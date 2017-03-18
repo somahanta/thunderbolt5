8aa1c0ca54b0258cfcdec5fb5639268dd0978a68
Soubhik <soubhik.sm1@gmail.com>
2017-03-18 09:58:45 +0000

    Modified static files and templates, Team photos added

--- effi/settings.py
+++ effi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f=3wzlb8l#oum0h$#+_cl5zrj-e#6_ybtv&hnyc(e43wh#mdd6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smsoubhik.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- website/static/website/javascript/script.js
+++ website/static/website/javascript/script.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
 	$('.bolt').css("border-radius","125px");
-	$('.bolt').css("transform","rotate(20deg)")
-	
 });
